05a86231427274ad55a9a8a30122f6fa6d5fffda
thiagogcr <hamburguer_de_ferro@hotmail.com>
2013-10-05 01:28:41 -0300

    Thiago

---
+++ Consultas/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models;
+using System.IO;
+
+namespace BootstrapMvcSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string TempPath = @"C:\Users\Thiago\Desktop\Teste";
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
+        {
+            foreach (HttpPostedFileBase file in files)
+            {
+                string filePath = Path.Combine(Server.MapPath("~/Content/temp/"), file.FileName);
+                System.IO.File.WriteAllBytes(filePath, ReadData(file.InputStream));
+            }
+
+            return Json("All files have been successfully stored.");
+        }
+
+        private byte[] ReadData(Stream stream)
+        {
+            byte[] buffer = new byte[16 * 1024];
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+
+                return ms.ToArray();
+            }
+        }
+    }
+}
