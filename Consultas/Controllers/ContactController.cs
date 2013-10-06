26364968a6fd8cdb9a23961316d69eaaa951d54d
startup2013mtp <startup2013mtp@outlook.com>
2013-10-05 22:34:19 -0300

    Matheus
    vai se fuder babaca

--- Consultas/Controllers/ContactController.cs
+++ Consultas/Controllers/ContactController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Framawork;
 
 namespace Consultas.Controllers
 {
@@ -14,6 +15,17 @@ namespace Consultas.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult enviaDadosContato(FormCollection form)
+        {
+            string nome = form["txtNome"];
+            string email = form["txtEmail"];
+            string estado = form["slcEstado"];
+            string mensagem = form["txtMsg"];
+            Mail.enviaDadosContato(nome, email, mensagem);
+            return RedirectToAction("Index");
+
         }
 
     }
--- Framawork/Mail.cs
+++ Framawork/Mail.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Mail;
@@ -12,20 +13,32 @@ namespace Framawork
         public static void enviaEmail() {
             System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage();
             msg.Subject = "Solicitação de oramento";
-            msg.From = new System.Net.Mail.MailAddress("tizinho29@gmail.com");
-            msg.To.Add(new System.Net.Mail.MailAddress("matheusny2006@gmail.com"));
+            msg.From = new System.Net.Mail.MailAddress("matheusny2006@gmail.com");
+            msg.To.Add(new System.Net.Mail.MailAddress("tizinho29@gmail.com"));
             msg.Body = TemplateHtml.mountEmail();
             msg.IsBodyHtml = true;
             System.Net.Mail.SmtpClient smpt = new System.Net.Mail.SmtpClient();
             smpt.Host = "smtp.gmail.com";
             smpt.Port = 587;
             smpt.EnableSsl = true;
-            /*
-             * VC ACHA QUE EU VOU DEXAR A SENHA DO MEU EMAIL AQUI NÉ TECAO
-             * HAHAHAHAHAHAHAH VAI CAGAR SÔ
-             */
-            smpt.Credentials = new System.Net.NetworkCredential("", "");
+            smpt.Credentials = new System.Net.NetworkCredential("matheusny2006@gmail.com", "gmailchains2013");
             smpt.Send(msg);
+        }
+        public static void enviaDadosContato(string nome, string email, string mensagem)
+        {
+            System.Net.Mail.MailMessage msg = new MailMessage();
+            msg.Subject = "Dúvida" + nome;
+            msg.Body = mensagem;
+            msg.From = new MailAddress(email);
+            msg.To.Add(new MailAddress("tizinho29@gmail.com"));
+            
+            System.Net.Mail.SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.Credentials = new NetworkCredential("matheusny2006@gmail.com", "gmailchains2013");
+            smtp.Send(msg);
+
         }
     }
 }
